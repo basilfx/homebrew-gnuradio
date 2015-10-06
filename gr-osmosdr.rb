22e175e1c00398431e06fa2948266018457e1750
Bas Stottelaar <basstottelaar@gmail.com>
2015-10-06 21:15:12 +0200

    Use gnuradio dependencies.

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -9,38 +9,25 @@ class GrOsmosdr < Formula
   depends_on 'gnuradio'
   depends_on 'librtlsdr'
 
-  # cheetah starts here
-  resource "Markdown" do
-    url "https://pypi.python.org/packages/source/M/Markdown/Markdown-2.4.tar.gz"
-    sha256 "b8370fce4fbcd6b68b6b36c0fb0f4ec24d6ba37ea22988740f4701536611f1ae"
-  end
-
-  resource "Cheetah" do
-    url "https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
-    sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
-  end
-  # cheetah ends here
-
   def install
-    ENV["CHEETAH_INSTALL_WITHOUT_SETUPTOOLS"] = "1"
-    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-
-    res = %w[Markdown Cheetah]
-    res.each do |r|
-      resource(r).stage do
-        system "python", *Language::Python.setup_install_args(libexec/"vendor")
-      end
-    end
-
-    args = std_cmake_args
+    ENV.prepend_create_path "PYTHONPATH", "#{gnuradio_path}/libexec/vendor/lib/python2.7/site-packages"
 
     mkdir "build" do
-      system "cmake", "..", *args
+      system "cmake", "..", *std_cmake_args << "-DPYTHON_LIBRARY=#{python_path}/Frameworks/Python.framework/"
       system "make"
       system "make", "install"
     end
+  end
 
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+  def python_path
+    python = Formula.factory('python')
+    kegs = python.rack.children.reject { |p| p.basename.to_s == '.DS_Store' }
+    kegs.find { |p| Keg.new(p).linked? } || kegs.last
+  end
+
+  def gnuradio_path
+    gnuradio = Formula.factory('gnuradio')
+    kegs = gnuradio.rack.children.reject { |p| p.basename.to_s == '.DS_Store' }
+    kegs.find { |p| Keg.new(p).linked? } || kegs.last
   end
 end
