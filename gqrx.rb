36fc8137ddf37cc2a0af1f17f9046b444950d3e3
Bas Stottelaar <basstottelaar@gmail.com>
2015-10-06 21:15:55 +0200

    Cleanup comments.

--- gqrx.rb
+++ gqrx.rb
@@ -1,27 +1,19 @@
 require 'formula'
 
-# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-# from: https://github.com/xlfe/homebrew-gnuradio
-
 class Gqrx < Formula
   homepage 'https://github.com/csete/gqrx'
   head 'https://github.com/csete/gqrx.git', :branch => 'master'
 
   depends_on 'cmake' => :build
   depends_on 'qt'
-#brew install --with-c+11 --universal boost
   depends_on 'boost'
   depends_on 'gnuradio'
   depends_on 'gr-osmosdr'
 
-  def patches
-    #patch to compile to binary, comment out pulse audio and link boost correctly
-    DATA
-  end
+  patch :DATA
 
   def install
-    system "qmake -set PKG_CONFIG /usr/local/bin/pkg-config"      
+    system "qmake -set PKG_CONFIG /usr/local/bin/pkg-config"
     system "qmake -query"
     system "qmake gqrx.pro"
     system "make"
